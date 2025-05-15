26ab79a2ef886ba3750f5443b6b374261bb5d1c7
Khrystyna Konepud <kristina.konepud@knu.ua>
2025-05-15 15:49:23 +0300

    Created models and db

--- Restaurant/models/beverage.py
+++ Restaurant/models/beverage.py
@@ -2,30 +2,24 @@ from django.db import models
 from Restaurant.models.category import Category
 
 
-class Dish(models.Model):
+class Beverage(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_visible = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
-    weight = models.DecimalField(max_digits=6, decimal_places=2)
-    ingredients = models.TextField()
-    photo = models.ImageField(upload_to='dishes/')
-
+    volume = models.DecimalField(max_digits=6, decimal_places=2)
+    is_alcoholic = models.BooleanField(default=False)
+    ingredients = models.TextField(blank=True, null=True)
+    photo = models.ImageField(upload_to='beverages/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
     sort = models.PositiveIntegerField(default=0)
-    is_vegetarian = models.BooleanField(default=False)
-    is_spicy = models.BooleanField(default=False)
-    cuisine_type = models.CharField(max_length=50, default='Азіатська')
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['sort']
-        db_table = 'dish'
+        db_table = 'beverage'
 
     def __str__(self):
         return self.name
-
